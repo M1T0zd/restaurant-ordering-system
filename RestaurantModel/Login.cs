a2993d0423f29bdc7c590a470526ef37b18cb7fa
M1T0zd <34724179+M1T0zd@users.noreply.github.com>
2019-05-24 21:46:40 +0200

    Initialized Program

---
+++ RestaurantModel/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantModel
+{
+    public class Login
+    {
+        public string username;
+        public string password;
+        public string adminStatus;
+
+    }
+}
---
+++ Restaurant_Logic/Login_Service.cs
@@ -0,0 +1,57 @@
+﻿using Restaurant_DAL;
+using RestaurantModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Restaurant_Logic
+{
+    class Login_Service
+    {
+        Login_DAO login_db = new Login_DAO();
+        public List<Login> GetLogin()
+        {
+            try
+            {
+                List<Login> logins = login_db.Db_Get_All_Login();
+                return logins;
+            }
+            catch (Exception e)
+            {
+                List<Login> logins = new List<Login>();
+                Login a = new Login();
+                a.username = "blabla";
+                a.password = "zxcv";
+
+                Login b = new Login();
+                b.username = "qwee";
+                b.password = "asdf";
+
+                ErrorLogging(e);
+
+                return logins;
+            }
+        }
+        public static void ErrorLogging(Exception e)
+        {
+            string strPath = @"D:\Prins\Log.txt";
+            if (!File.Exists(strPath))
+            {
+                File.Create(strPath).Dispose();
+            }
+            using (StreamWriter sw = File.AppendText(strPath))
+            {
+                sw.WriteLine("=============Error Logging ===========");
+                sw.WriteLine("===========Start============= " + DateTime.Now);
+                sw.WriteLine("Error Message: " + e.Message);
+                sw.WriteLine("Stack Trace: " + e.StackTrace);
+                sw.WriteLine("===========End============= " + DateTime.Now);
+
+            }
+        }
+    }
+}
